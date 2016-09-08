96c768b8d4f7ed139fecc49b67bf3a65f9b8ef48
VectorAnalytics <Marcia@wifi_stu-10-38-86-19.uncc.edu>
2016-09-08 14:27:21 -0400

    Updated on 9/1

--- Clean_Ut_Claims_Fr_Walid_CSV.py
+++ Clean_Ut_Claims_Fr_Walid_CSV.py
@@ -1,33 +1,34 @@
 #!/usr/bin/env python
 # -- coding: utf-8 -- 
-# This code takes a Walid claim csv file, filters for just utility patents,
+
+# This code takes the Walid claim csv file created from SOLR, filters for just utility patents,
 #   takes only the first 6500 characters of the claim, creates a 9 char string patnum,
 #   and writes those lines back to a new cvs file.
 
 import csv
 import re
 import sys
-#Had to add this sys.maxsize statement because rows from Walid are so long
+#Had to add this sys.maxsize statement because rows from SOLR are so long.
 csv.field_size_limit(sys.maxsize)
 
 #Opening the file I write to before getting in the "for" loop.
-outfile = open('/Users/Marcia/OneDrive/DSBA_6880/Parsed_Patent_Files/WalidFiles/claims_6500_2015_ut.csv', 'w')
+outfile = open('/Users/Marcia/OneDrive/DSBA_6880/Parsed_Patent_Files/WalidFiles/claims_6500_2013_ut.csv', 'w')
 
 #Debugging errors lead to need for "utf-8" and "ignore" in the open statement.
 #The ignore due to some pats lacking a claim field, I think.
-with open('/Users/Marcia/OneDrive/DSBA_6880/Parsed_Patent_Files/WalidFiles/claims_2015.csv', \
+with open('/Users/Marcia/OneDrive/DSBA_6880/Parsed_Patent_Files/WalidFiles/claims_2013.csv', \
     encoding='utf-8', errors='ignore' ) as claimfile:
     claimlist = csv.reader(claimfile)
     #Skip the header row    
     next(claimlist)    
     for row in claimlist:
-        #Used next line to only pull utility patents, but didn't seem to reduce filesize.
+        #Used next line of code to only pull utility patents, but didn't seem to reduce filesize.
         if str(row[0])[2:3] in '6789' and str(row[0])[9:10] == 'B':        
         #Shortened claim to 6500 chars which is approx 1000 words to make filezise more manageable.        
-        #And Walid data is riddled with \n line breaks between the quotes,  
-        #which open ok in EXCEL but lead to issues here.        
+        #And SOLR data is riddled with \n line breaks between the quotes,  
+        #   which open ok in EXCEL but lead to issues here, so I replaced \n with a blank.        
             claim_c= row[1][0:6500].replace('\n', ' ') 
-        #Was getting an error about "ascii codec can't encode character" so kept only alpha's
+        #Was getting an error about "ascii codec can't encode character" so kept only alpha's.
             claim_ao = re.sub("[^a-zA-Z]", " ", claim_c) 
         #Grabbed just patnumber from Walid ID num, deleting US and TypeCode.        
             pat_num=str(row[0])[2:9] 
--- Combine_Plist_Clist_V1.py
+++ Combine_Plist_Clist_V1.py
@@ -1,38 +1,46 @@
 #!/usr/bin/env python
 # -- coding: utf-8 -- 
 import csv
-from timeit import default_timer as timer
 
-start = timer()
+#This program match merges a list of patents under study (from a particular year)
+#    with a year's worth of cleaned claim data from Walid's SOLR server.
 
-#Opening the file I write to before getting into the "for" loop.
+#Was using timer to understand processing time for various filesizes.
+#from timeit import default_timer as timer
+
+#start = timer()
+
+#I have various "outfile" and "with open" statements, based on if running on MAC or at WW432 and if test or real file.
+
+#Name and open the file I want to saved my matched merge results to.
 #outfile = open("/Users/Marcia/OneDrive/DSBA_6880/Parsed_Patent_Files/Claim_Files/claim_20_200.csv", 'w')
-outfile = open("/Users/mprice79/claim_21_200.csv", 'w')
+outfile = open("/Users/Marcia/OneDrive/DSBA_6880/Parsed_Patent_Files/Claim_Files/pat_list_2013_match_to_claims.csv", 'w')
+#outfile = open("/Users/mprice79/claim_21_200.csv", 'w')
+
+#Get list of patents that I want to find matching claims for.
 #Debugging errors lead to need for "utf-8" and "ignore" in the open statement.
 #The ignore due to some pats lacking a claim field, I think.
 
-#Get list of patents that you want to find matching claims for.
-with open('/Users/mprice79/patonly_test_2015.csv', \
+#with open('/Users/mprice79/patonly_test_2015.csv', \
 #with open('/Users/Marcia/OneDrive/DSBA_6880/Parsed_Patent_Files/Study_Abs_160513/pat_test_2015.csv', \
+with open('/Users/Marcia/OneDrive/DSBA_6880/Parsed_Patent_Files/Study_Abs_160513/pat_list_2013.csv', \
     encoding='utf-8', errors='ignore' ) as patfile:   
     patlist = csv.DictReader(patfile)
     for pat in patlist:           
-#Get list of claims under study
-        with open('/Users/mprice79/claims_6500_200.csv', \
-        #with open('/Users/Marcia/OneDrive/DSBA_6880/Parsed_Patent_Files/WalidFiles/claims_6500_20000.csv', \
+#Get list of claims under study, this is Walid data after reformatting pat number
+#   and grabbing only first 6500 alpha chars of claim.
+        #with open('/Users/mprice79/claims_6500_200.csv', \
+        with open('/Users/Marcia/OneDrive/DSBA_6880/Parsed_Patent_Files/WalidFiles/claims_6500_2013_ut.csv', \
             encoding='utf-8', errors='ignore' ) as claimfile:
                 fieldnames = ['pat_num', 'claim']    
                 claimlist = csv.DictReader(claimfile, fieldnames=fieldnames)
                 for patc in claimlist:
                     if pat['pat_num'] == patc['pat_num']:
-                        #print(patc['pat_num'], patc['claim'])
-                        #Claim=[patc['pat_num'], patc['claim'], pat['pat_set']]
-                        Claim=[patc['pat_num'], patc['claim']]
-                        #print(pat_list) 
+                        Claim=[patc['pat_num'], patc['claim'], pat['pat_set']]
                         wr =  csv.writer(outfile, lineterminator='\n', dialect='excel')
                         #Write one row at a time to the open outfile.        
                         wr.writerow(Claim) 
 #Needed to close outfile officially as I didn't use "with open" to open it.
 outfile.close()
-end = timer()
-print(end-start)   
+#end = timer()
+#print(end-start)   
